1fc8f69f98aaf54b3550d19282b705c8d4c00e3d
Emery Conrad <emery.conrad@chicagotrading.com>
2025-12-17 04:38:13 -0600

    tweaks to boot up CTC

--- clingwrapper/src/clingwrapper.cxx
+++ clingwrapper/src/clingwrapper.cxx
@@ -177,9 +177,18 @@ class ApplicationStarter {
 public:
     ApplicationStarter() {
         std::lock_guard<std::recursive_mutex> Lock(InterOpMutex);
-        if (!Cpp::LoadDispatchAPI(
-                CPPINTEROP_DIR
-                "/lib/libclangCppInterOp" CMAKE_SHARED_LIBRARY_SUFFIX)) {
+
+	std::string CppInterOpLib;
+        if(char *lib = getenv("CPPINTEROP_LIB_PATH")) {
+            CppInterOpLib = lib;
+            std::cerr << "[cppyy-backend] loading CppInterOp from " << CppInterOpLib << std::endl;
+        }
+#if defined(CPPINTEROP_DIR) && defined(CMAKE_SHARED_LIBRARY_SUFFIX)
+        else
+            CppInterOpLib = CPPINTEROP_DIR "/lib/libclangCppInterOp" CMAKE_SHARED_LIBRARY_SUFFIX;
+#endif
+
+        if (!Cpp::LoadDispatchAPI(CppInterOpLib.c_str())) {
             std::cerr << "[cppyy-backend] Failed to load CppInterOp" << std::endl;
             return;
         }
@@ -189,34 +198,19 @@ public:
             Interp = existingInterp;
         }
         else {
-#ifdef __arm64__
-#ifdef __APPLE__
-            // If on apple silicon don't use -march=native
             std::vector<const char *> InterpArgs({"-std=c++17"});
-#else
-            std::vector<const char *> InterpArgs(
-                {"-std=c++17", "-march=native"});
+
+            // If on apple silicon don't use -march=native
+#if !defined(__arm64__) || !defined(__APPLE__)
+            InterpArgs.push_back("-march=native");
 #endif
-#else
-            std::vector <const char *> InterpArgs({"-std=c++17", "-march=native"});
-#endif
+
             char *InterpArgString = getenv("CPPINTEROP_EXTRA_INTERPRETER_ARGS");
 
             if (InterpArgString)
               push_tokens_from_string(InterpArgString, InterpArgs);
 
-#ifdef __arm64__
-#ifdef __APPLE__
-            // If on apple silicon don't use -march=native
-            Interp = Cpp::CreateInterpreter({"-std=c++17"}, /*GpuArgs=*/{});
-#else
-            Interp = Cpp::CreateInterpreter({"-std=c++17", "-march=native"},
-                                            /*GpuArgs=*/{});
-#endif
-#else
-            Interp = Cpp::CreateInterpreter({"-std=c++17", "-march=native"},
-                                            /*GpuArgs=*/{});
-#endif
+            Interp = Cpp::CreateInterpreter(InterpArgs, /*GpuArgs=*/{});
         }
 
         // fill out the builtins
@@ -248,7 +242,23 @@ public:
         Cpp::AddIncludePath((ClingSrc + "/tools/cling/include").c_str());
         Cpp::AddIncludePath((ClingSrc + "/include").c_str());
         Cpp::AddIncludePath((ClingBuildDir + "/include").c_str());
+
+        if(char *inc = getenv("CPPINTEROP_INCLUDE_PATH"))
+            Cpp::AddIncludePath(inc);
+
+#ifdef CPPINTEROP_DIR
         Cpp::AddIncludePath((std::string(CPPINTEROP_DIR) + "/include").c_str());
+#endif
+
+#ifdef CTC_BUILD_HACKS
+	std::vector<std::string> ipaths;
+	Cpp::GetIncludePaths(ipaths, true, false);
+	for(auto path: ipaths) {
+		std::cerr << path << std::endl;
+	}
+	std::cerr << std::endl;
+#endif
+
         Cpp::LoadLibrary("libstdc++", /* lookup= */ true);
 
         // load frequently used headers
